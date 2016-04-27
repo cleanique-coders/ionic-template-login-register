42f500e6059c45abc73282155aabf305df3ad0c5
Cleanique Coders <cleanique.coders@gmail.com>
2016-04-27 21:41:35 +0800

    initial

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('profileCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('registerCtrl', function($scope) {
+
+})
+ 
